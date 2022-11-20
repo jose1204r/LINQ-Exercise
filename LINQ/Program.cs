356bd826e69fff02e604ea6f9cff69b9bb3536b1
jose <jrodriguez@itsolutionspcllc.com>
2022-11-19 21:38:38 -0500

    LINQ-Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ
+
+
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "PacMan", "GOD of War", "Call of Duty", "Alien", "Halo Infinity" };
+            videoGames.Where (names => names == "");
+
+            Console.WriteLine("************GAMES LIST ***************");
+
+            foreach (var game in videoGames)
+            {
+                
+                
+                Console.WriteLine(game);
+
+
+            }
         }
     }
 }
